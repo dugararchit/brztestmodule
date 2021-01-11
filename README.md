08e0a1df439f639d70a721029e8710bbb96fe4b4
Archit Dugar <archit.dugar@thepsi.com>
2021-01-11 12:53:17 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# what is this ? 
+
+This is a test module for brz
---
+++ index.js
@@ -0,0 +1,5 @@
+function testModule(calculator){
+    console.log("hitting it from module");
+}
+
+module.exports.testModule = testModule;
